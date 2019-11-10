f203df5f9f01e7bb54aca73fde13f4864e1809b1
David L. Inman <davidlevere@gmail.com>
2019-11-10 14:27:33 -0500

    Added list of tokens

--- data.mli
+++ data.mli
@@ -1,3 +1,20 @@
 
 type token = 
     | String of string
+    | Int of int
+    | Float of float
+    | Symbol of string
+    | Dot
+    | RParen
+    | LParen
+    | RCurl
+    | LCurl
+    | Orbar
+    | Colon
+    | SemiColon
+    | RAngle
+    | LAngle
+    | Comma
+    | RightArrow 
+    | Equal 
+

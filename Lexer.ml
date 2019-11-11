94a27aeab01ab6e3edc33d2504c828ef81a60fa0
David L. Inman <davidlevere@gmail.com>
2019-11-10 22:39:30 -0500

    inc prog

--- Lexer.ml
+++ Lexer.ml
@@ -3,11 +3,18 @@ open Data
 
 exception LexError of int * char * string
 
-let lex ( reader : < current : char
-          ; index : int
-          ; move_next : bool
-          ; look_ahead : int -> char option > )
-          
-          = []
+let lex ( input : < current : char
+                  ; index : int
+                  ; move_next : bool
+                  ; look_ahead : int -> char option > ) =
 
+    let ret = ref [] in
+    while input#move_next do
+        match input#current with
+        | c when c is_whitespace c -> ()
+        | '(' -> ret := LParen :: !ret
+        | ')' -> ret := RParen :: !ret
+        | _ -> raise (LexError (input#index, input#current, "Unknown character encountered"))
+    done
+    ; List.rev !ret
 

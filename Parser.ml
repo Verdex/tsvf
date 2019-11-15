8b2ae46c065bb7c79f923dd12b35ff06e4632e33
David L. Inman <davidlevere@gmail.com>
2019-11-14 22:00:45 -0500

    fix issue with parse signature

--- Parser.ml
+++ Parser.ml
@@ -4,8 +4,8 @@ open Data
 exception ParseError of string
 
 let parse (input : < current : token 
-            ; move_next : bool
-            ; look_ahead : int -> token option >) =
+                   ; move_next : bool
+                   ; look_ahead : int -> token option >) =
 
     Module
 
